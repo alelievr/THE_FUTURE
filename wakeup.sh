ee1905095f304cd971ad7631b22d7d4def640dd4
alelievr <alelievr@student.42.fr>
2017-06-25 15:24:38 +0200

    added wakeup test script

---
+++ wakeup.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+osascript -e '
+tell application "System Events"
+key code 123 -- left arrow
+key code 36 -- return
+key code 14 -- e
+key code 1  -- s
+key code 35 -- p
+key code 31 -- o
+key code 15 -- r
+key code 17 -- t
+key code 21 -- 4
+key code 19 -- 2
+key code 36 -- return 
+end'
